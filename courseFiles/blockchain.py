7f103d74900823da5f2156c09930b7fbdc8b9402
saurabhsigma <saurabh962003@gmail.com>
2025-04-18 23:18:17 +0530

    built the blockchain

--- courseFiles/blockchain.py
+++ courseFiles/blockchain.py
@@ -59,4 +59,37 @@ class Blockchain:
             previous_block = block
             block_index+=1
         return True
+
+
+
 # Part 2 - Mining our Blockchain
+
+
+# creating a Web App
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new Block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof,previous_hash)
+    response = {'message' : 'congrats you just mined a block',
+                'index': block['index'],
+                'timestampt': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# getting the full blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain':blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
